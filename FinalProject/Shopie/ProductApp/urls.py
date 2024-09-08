90d93e743846e900d7f01174468ac3a1a9531c92
lailaebrahim <ebrahimlaila815@gmail.com>
2024-09-08 22:51:55 +0300

    Add product and category views to display all products, certain product, all categories with their most recent added three products and all the products in a category

--- FinalProject/Shopie/ProductApp/urls.py
+++ FinalProject/Shopie/ProductApp/urls.py
@@ -3,4 +3,8 @@ from .views import *
 
 urlpatterns = [
     path('home/', home, name="home"),
+    path('products/', AllProductsView, name="products"),
+    path('category/', CategoryProductsView, name="category_products"),
+    path('products/category/', CategoryView, name="category"),
+    path('product/<int:pk>/', ProductDetailView.as_view(), name="product")
 ]
--- FinalProject/Shopie/ProductApp/views.py
+++ FinalProject/Shopie/ProductApp/views.py
@@ -1,7 +1,42 @@
 from django.shortcuts import render
-from .models import Product
+from django.core.paginator import Paginator
+from django.views.generic import DetailView
+from .models import Product, Category
 
 
 def home(request):
     products = Product.objects.all()[:9]
     return render(request, 'ProductApp/home.html', context={"products": products})
+
+
+def AllProductsView(request):
+    products = Product.objects.all()
+    products_pagintaor_obj = Paginator(products, 6)
+    page_num = request.GET.get('page')
+    products = products_pagintaor_obj.get_page(page_num)
+    return render(request, 'ProductApp/products.html', context={"products": products})
+
+def CategoryProductsView(request):
+    categories = Category.objects.all()
+    categories_paginator_obj = Paginator(categories, 4)
+    page_num = request.GET.get('page')
+    categories_page = categories_paginator_obj.get_page(page_num)
+    category_products = {}
+    for category in categories_page:
+        category_products[category] = Product.objects.filter(category=category)[:3]
+    return render(request, 'ProductApp/category_products.html', context={"category_products": category_products, "categories_page": categories_page})
+
+def CategoryView(request):
+    category = request.GET.get('category')
+    category = Category.objects.get(id=category)
+    category_products = Product.objects.filter(category=category.id)
+    category_products_pag_obj = Paginator(category_products, 6)
+    page_num = request.GET.get('page')
+    category_products_page = category_products_pag_obj.get_page(page_num)
+    return render(request, 'ProductApp/category.html', context={"category_products": category_products_page, "category": category})
+
+
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = 'ProductApp/product.html'
+    context_object_name = 'product'
